580f9bae71cf8ef03e28572ac2c507a2546ebc47
Phạm Thành Nam <94697323+ThanhhNamm@users.noreply.github.com>
2023-08-29 23:28:00 +0700

    Update update_profile.java
    - Đã fix được thêm ảnh (bao gồm: Avatar, Cover Photo)
    - Thêm auto Thông tin nổi bật
    - Đang hoàn thiện Thêm thông tin cá nhân (Vướng chọn tỉnh - dropdown và Chọn Datetime)

--- update_profile.java
+++ update_profile.java
@@ -15,6 +15,8 @@ import java.awt.datatransfer.StringSelection;
 import java.awt.event.KeyEvent;
 import java.time.Duration;
 import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.support.ui.Select;
+
 
 public class update_profile {
 
@@ -44,14 +46,15 @@ public class update_profile {
     }
 
 
-    // Update avatar & cover photo
+    // Update avatar
     @Test
     public void Tescase1() throws AWTException, InterruptedException {
 
-        wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/app-root/app-main/div/div[2]/app-user/div/app-edit/div/app-user-banner/div[1]/div[2]/button"))).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/app-root/app-main/div/div[2]/app-user/div/app-edit/div/app-user-banner/div[2]/div/div[1]/div/button/div"))).click();
 
         String filePath = "C:\\Users\\Hello\\OneDrive\\Máy tính\\AnhThe.jpg";
-//      //Sử dụng Robot để điều khiển hệ thống
+
+        //Sử dụng Robot để điều khiển hệ thống
         Robot robot = new Robot();
 
         // Đặt đường dẫn tệp vào clipboard
@@ -68,48 +71,96 @@ public class update_profile {
         robot.keyPress(KeyEvent.VK_ENTER);
         robot.keyRelease(KeyEvent.VK_ENTER);
 
-        Thread.sleep(10000);
-        //chromeDriver.findElement(By.xpath("//*[@id=\"cdk-overlay-1\"]/nz-modal-container/div/div/button/span/button")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"cdk-overlay-0\"]/nz-modal-container/div/div/div/app-image-resize/div/button"))).click();
 
-
-        JavascriptExecutor jsExecutor = (JavascriptExecutor) chromeDriver;
-        WebElement hiddenElement = (WebElement) jsExecutor.executeScript(
-                "return document.evaluate(//*[@id=\"cdk-overlay-2\"]/nz-modal-container/div/div/div/app-image-resize/div/button, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;");
-
-        // Thực hiện hành động click trên phần tử ẩn
-        jsExecutor.executeScript("arguments[0].click();", hiddenElement);
-
-
-        //wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"cdk-overlay-3\"]/nz-modal-container/div/div/div/app-image-resize/div/button"))).click();
-
-
-//        Thread.sleep(10000);
-//        // Định vị phần tử <input> để tải lên tệp ảnh
-//        WebElement fileInput = chromeDriver.findElement(By.xpath("/html/body/app-root/app-main/div/div[2]/app-user/div/app-edit/div/app-user-banner/div[1]/div[2]/button"));  // Thay thế bằng cách định vị thích hợp
-//        fileInput.click();
-//
-//        // Lấy đường dẫn tới tệp ảnh bạn muốn tải lên trên máy tính
-//        String filePath = "C:\\Users\\Hello\\Downloads\\309304357_1583129602195662_4150601600369615547_n.png";
-//
-//        // Gửi đường dẫn tệp ảnh đến phần tử <input> để tải lên - Sử dụng sendKey - tối ưu code nhất nhưng chưa fix được!
-//        wait.until(ExpectedConditions.presenceOfAllElementsLocatedBy(By.xpath("/html/body/app-root/app-main/div/div[2]/app-user/div/app-edit/div/app-user-banner/div[1]/div[2]/button")));
-//        fileInput.sendKeys(filePath);
 
     }
 
 
-
+    // Update Cover photo ==> oke
     @Test
-    public void Testcase2() throws InterruptedException {
+    public void Testcase2() throws InterruptedException, AWTException {
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/app-root/app-main/div/div[2]/app-user/div/app-edit/div/app-user-banner/div[1]/div[2]/button"))).click();
+
+        String expectedValue = "C:\\Users\\Hello\\OneDrive\\Document\\Hình ảnh\\Hình ảnh\\Ảnh mẫu\\368345627_238854679125976_5345742738850022693_n.jpg";
+
+        //Sử dụng Robot để điều khiển hệ thống
+        Robot robot = new Robot();
+
+        // Đặt đường dẫn tệp vào clipboard
+        StringSelection stringSelection = new StringSelection(expectedValue);
+        Toolkit.getDefaultToolkit().getSystemClipboard().setContents(stringSelection, null);
+        Thread.sleep(1000);
+        // Gửi phím tắt Ctrl+V để dán đường dẫn tệp
+        robot.keyPress(KeyEvent.VK_CONTROL);
+        robot.keyPress(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_V);
+        robot.keyRelease(KeyEvent.VK_CONTROL);
+        Thread.sleep(1000);
+        // Gửi phím Enter để xác nhận tải lên
+        robot.keyPress(KeyEvent.VK_ENTER);
+        robot.keyRelease(KeyEvent.VK_ENTER);
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id=\"cdk-overlay-0\"]/nz-modal-container/div/div/div/app-image-resize/div/button"))).click();
+
+
+//        Assert.assertEquals(actualValue, expectedValue);
+
+
+
+    }
+
+    // Update Thông tin nổi bật ==> oke
+    @Test
+    public void Testcase3() throws InterruptedException {
+
+        String actualValue = "Đây được tạo từ automation test";
 
         wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/app-root/app-main/div/div[2]/app-user/div/app-edit/div/div/app-user-highlight/div/div/div[2]/button"))).click();
+        Thread.sleep(2000);
+        chromeDriver.findElement(By.xpath("//*[@id=\"formly_2_ui-quill_hightlight_0\"]/div[2]/div[1]")).sendKeys(actualValue);
+        Thread.sleep(2000);
+        chromeDriver.findElement(By.xpath("//*[@id=\"cdk-overlay-0\"]/nz-modal-container/div/div/div/app-user-highlight-modal/div/div/button[2]")).click();
 
-        chromeDriver.findElement(By.xpath("//*[@id=\"formly_4_ui-quill_hightlight_0\"]/div[2]/div[1]")).sendKeys("Đây là thông tin được tạo do auto test");
+        String expectedValue = chromeDriver.findElement(By.xpath("/html/body/app-root/app-main/div/div[2]/app-user/div/app-edit/div/div/app-user-highlight/div/div[2]/div")).getText();
+        Assert.assertNotEquals(actualValue,expectedValue,"True");
+    }
 
-        Thread.sleep(5000);
+    // Update Thông tin cá nhân
+    @Test
+    public void Testcase4() throws InterruptedException {
 
-        Alert alert = chromeDriver.switchTo().alert();
-        alert.sendKeys("Co chó này");
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("/html/body/app-root/app-main/div/div[2]/app-user/div/app-edit/div/div/app-user-basic-edit/div/div[1]/div[2]/button"))).click();
+
+        Thread.sleep(400);
+        chromeDriver.findElement(By.xpath("//*[@id=\"formly_12_ui-input_address_0\"]")).sendKeys("Địa chỉ tạo bằng automation test");
+
+        // update ngày sinh ==> chưa được
+        Thread.sleep(400);
+        WebElement updateDob = chromeDriver.findElement(By.xpath("//*[@id=\"formly_8_ui-datepicker_dob_0\"]/div/input"));
+        updateDob.sendKeys("01/09/2010");
+        updateDob.sendKeys(Keys.ENTER);
+
+
+        // Update Gender ==> oke
+        Thread.sleep(400);
+        WebElement updateGender = chromeDriver.findElement(By.xpath("//*[@id=\"formly_10_ui-select_gender_0\"]/nz-select-top-control/nz-select-search/input"));
+        updateGender.sendKeys("Nam");
+        updateGender.sendKeys(Keys.ENTER);
+
+        // Update Province ==> chưa được
+
+        Thread.sleep(400);
+        WebElement updateCity = chromeDriver.findElement(By.xpath("//*[@id=\"formly_12_ui-select_city_3\"]/nz-select-top-control/nz-select-item/div"));
+        updateCity.sendKeys("Tỉnh Lào Cai");
+        updateCity.sendKeys(Keys.ENTER);
+
+        // Update District ==> chưa được
+        Thread.sleep(200);
+        Select updateDistrict = new Select(chromeDriver.findElement(By.xpath("//*[@id=\"formly_12_ui-select_district_4\"]/nz-select-top-control/nz-select-item")));
+        updateDistrict.selectByVisibleText("Huyện Si Ma Cai");
+
 
     }
 
